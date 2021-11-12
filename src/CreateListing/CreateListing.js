2acead81366fc6f37c98f78c77635ca51d334195
the-96 <godcantpee@gm>
2021-11-11 16:29:22 -0800

    frontend added for createListing

--- src/CreateListing/CreateListing.js
+++ src/CreateListing/CreateListing.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './CreateListing.css';
-const CreateListing = () => {
+const CreateListing = (props) => {
     // Only accessible by Admin
     // Create a Listing here
-    return (
-        <div>
-            Create Listing page
-        </div>
-    )
-}
+    const [value,setValue] = useState(props.name);
+    const handleChange = (e) => {
+        setValue(e.target.value);
+        console.log(e.target.value);
+    };
 
-export default CreateListing
+    return (
+        <form>
+            <label for='listingName'>Item for sale:</label>
+            <input type="text" id='listingName' name="listingName" placeholder='Product Name'/>
+            
+            <p>
+                <textarea value={value} onChange={handleChange} id='description' placeholder='Enter a Description . . .'name='description'/>
+            </p>
+            <p name='price'>
+                $ <input name="price" />
+            </p>
+            <label>
+                
+                <input type="file" name="imageFile" />
+            </label>
+
+            
+            <div class='row'>
+                <input type='submit' value='Submit' />
+            </div>
+
+        </form>
+    )
+};
+
+export default CreateListing;
