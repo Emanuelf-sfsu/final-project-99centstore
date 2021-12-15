0c33444d6cd56e4704f8531019c5c4df30a0a0f7
Kyle Chen <kylec641@gmail.com>
2021-12-14 22:36:39 -0800

    Set conditional routing of createListing and editListing for logged in admin

--- src/App.js
+++ src/App.js
@@ -9,18 +9,25 @@ import Registration from './Registration/Registration';
 import Container from 'react-bootstrap/Container'
 import NavbarComponent from './NavbarComponent';
 import { ToastContainer } from 'react-toastify';
+import { useSelector } from 'react-redux';
 
 const App = ({ store }) => {
+  const isLoggedIn = useSelector(state => state.userReducer.isLoggedIn);
+  const isAdmin = useSelector(state => state.userReducer.isAdmin);
+
   return (
     <Container className="App">
       <ToastContainer />
       <NavbarComponent />
       <Routes>
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/createListing" element={<CreateListing />} />
+        {(isLoggedIn && isAdmin) &&
+          <>
+            <Route path="/createListing" element={<CreateListing />} />
+            <Route path="/editListing/:id" element={<CreateListing />} />
+          </>
+        }
         <Route path="/viewListing/:id" element={<ViewListing />} />
-        <Route path="/editListing/:id" element={<CreateListing />} />
-
         <Route exact path='/listing' element={<Listings />} />
         <Route path="/" element={<Navigate replace to="/listing" />} />
         <Route path="/registration" element={<Registration store={store}/>} />
